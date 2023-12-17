bdb70af335cbc660fd2ad6497e35335169903ed8
NgHung111 <0306191226@caothang.edu.vn>
2023-12-17 17:39:43 +0700

    commit tieu de

--- resources/views/user/layouts/head.blade.php
+++ resources/views/user/layouts/head.blade.php
@@ -3,4 +3,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
 <link rel="icon" type="image/x-icon" href="{{ asset('assets/user/images/favicon.ico') }}">
-<title>Tên công ty</title>
+<title>HL Shoes Store</title>
